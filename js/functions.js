4073eab0da2872cb19ef14ae93453d45d7762dd5
PolarARR <angel-janus@hotmail.com>
2025-04-28 01:00:32 -0600

    finish search bar

--- js/functions.js
+++ js/functions.js
@@ -30,28 +30,160 @@ const createPokemonCard = (pokemon) => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon", {params: {limit: 151}})
+    axios.get("https://pokeapi.co/api/v2/pokemon", {params: {limit: 7000}})
         .then((response) => {
             const pokemonGrid = document.querySelector('.pokemons');
             const { data } = response;
-            const orederedPokemons = [];
+            const orderedPokemons = [];
             let i = 0;
 
             data.results.forEach((pokemon) => {
-                fetch(pokemon.url)
+                fetch(pokemon.url) 
                     .then((response) => response.json())
                     .then((pokemonData) => {
-                        orederedPokemons.push(pokemonData);
+                        orderedPokemons.push(pokemonData);
                         i++;
 
                         if (i === data.results.length) {
-                            orederedPokemons.sort((a, b) => a.id - b.id);
+                            orderedPokemons.sort((a, b) => a.id - b.id);
                         
 
-                        orederedPokemons.forEach ((pokemon) =>{
+                        orderedPokemons.forEach ((pokemon) =>{
                             const pokemonCard = createPokemonCard(pokemon);
                             pokemonGrid.appendChild(pokemonCard);
                         }); 
+
+                        const searchInput = document.getElementById("search-input");
+                        const searchIcon = document.getElementById("search-icon");
+                        const searchSection = document.querySelector(".search");
+
+                        searchInput.addEventListener("input", (event) => {
+                            const searchUl = document.querySelector("#suggestions");
+                            const searchedPokemon = orderedPokemons.filter(pokemon => 
+                                pokemon.name.toLowerCase().includes(event.target.value.toLowerCase())
+                            );                 
+                            let counter = 0;
+
+                            searchUl.innerHTML = "";
+
+                            if (event.target.value.toLowerCase() === "") { return; }
+
+                            searchedPokemon.forEach((pokemon) => {
+                                if (counter >= 5) { return; }
+
+                                const searchLi = document.createElement("li");
+                                const searchPokemonName = document.createElement("p");
+                                const searchPokemonImg = document.createElement("img");
+
+                                searchPokemonName.textContent = pokemon.name;
+                                searchPokemonImg.src = pokemon.sprites.front_default;
+                                searchPokemonImg.alt = pokemon.name;
+
+                                searchLi.appendChild(searchPokemonName);
+                                searchLi.appendChild(searchPokemonImg);
+                                searchUl.appendChild(searchLi);
+
+                                counter++;
+                            });
+
+                            switch (searchedPokemon.length) {
+                                case 4:
+                                    searchUl.classList.add("fourSuggestions");
+                                    break;
+                                case 3:
+                                    searchUl.classList.add("threeSuggestions");
+                                    searchUl.classList.remove("fourSuggestions");
+                                    break;
+                                case 2:
+                                    searchUl.classList.add("twoSuggestions");
+                                    searchUl.classList.remove("fourSuggestions", "threeSuggestions");
+                                    break;
+                                case 1:
+                                    searchUl.classList.add("oneSuggestion");
+                                    searchUl.classList.remove("fourSuggestions", "threeSuggestions", "twoSuggestions");
+                                    break;
+                                default:
+                                    searchUl.classList.remove("fourSuggestions", "threeSuggestions", "twoSuggestions", "oneSuggestion");
+                                    break;
+                            }
+                            
+                            searchUl.addEventListener("click", (event) => {
+                                const clickedPokemonName = event.target.querySelector("p").textContent;
+                                const clickedPokemon = orderedPokemons.find(pokemon => pokemon.name === clickedPokemonName);
+                                
+                                pokemonGrid.innerHTML = "";
+                                const clickedPokemonCard = createPokemonCard(clickedPokemon);
+                                pokemonGrid.appendChild(clickedPokemonCard);
+
+                                searchInput.value = clickedPokemonName;
+                                searchUl.innerHTML = "";
+                                searchUl.classList.remove("fourSuggestions", "threeSuggestions", "twoSuggestions", "oneSuggestion");
+                            });
+                        });
+
+                        searchInput.addEventListener("blur", () => {
+                            setTimeout(() => {
+                                const searchUl = document.querySelector("#suggestions");
+                                searchUl.innerHTML = "";
+                                searchUl.classList.remove("fourSuggestions", "threeSuggestions", "twoSuggestions", "oneSuggestion");    
+                            }, 100);
+                        });
+
+                        searchInput.addEventListener("keydown", (event) => {
+                            searchIcon.addEventListener("click", () => {
+                                pokemonGrid.innerHTML = "";
+    
+                                const searchedPokemon = orderedPokemons.filter(pokemon => 
+                                    pokemon.name.toLowerCase().includes(event.target.value.toLowerCase())
+                                );
+                                
+                                searchedPokemon.forEach((pokemon) => {
+                                    const searchedPokemonCard = createPokemonCard(pokemon);
+                                    pokemonGrid.appendChild(searchedPokemonCard);
+                                });
+
+                                if (pokemonGrid.innerHTML === "") {
+                                    const emptyMessage = document.createElement("h3");
+                                    emptyMessage.textContent = "No se encontró ningún pokémon con este nombre :(";
+                                    pokemonGrid.appendChild(emptyMessage);
+                                }
+
+                                searchInput.classList.add("searching");
+                                searchIcon.classList.add("searching");
+
+                                setTimeout(() => {
+                                    searchInput.classList.remove("searching");
+                                    searchIcon.classList.remove("searching");
+                                }, 500);
+                            });
+
+                            if (event.key === "Enter") {
+                                pokemonGrid.innerHTML = "";
+
+                                const searchedPokemon = orderedPokemons.filter(pokemon => 
+                                    pokemon.name.toLowerCase().includes(event.target.value.toLowerCase())
+                                );
+
+                                searchedPokemon.forEach((pokemon) => {
+                                    const searchedPokemonCard = createPokemonCard(pokemon);
+                                    pokemonGrid.appendChild(searchedPokemonCard);
+                                });        
+                                
+                                if (pokemonGrid.innerHTML === "") {
+                                    const emptyMessage = document.createElement("h3");
+                                    emptyMessage.textContent = "No se encontró ningún pokémon con este nombre :(";
+                                    pokemonGrid.appendChild(emptyMessage);
+                                }
+
+                                searchInput.classList.add("searching");
+                                searchIcon.classList.add("searching");
+
+                                setTimeout(() => {
+                                    searchInput.classList.remove("searching");
+                                    searchIcon.classList.remove("searching");
+                                }, 500);
+                            } 
+                        });
                     }
                 })
                 .catch((error) => {
